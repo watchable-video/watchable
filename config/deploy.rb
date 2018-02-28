246a31866ae835ca29822012a0a1127d8a4093e9
Ben Ubois <ben@benubois.com>
2018-02-28 05:54:03 -0800

    Deploy updates.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "tube"
 set :repo_url, "git@github.com:feedbin/#{fetch(:application)}.git"
 set :deploy_to, "/srv/apps/#{fetch(:application)}"
 set :rbenv_type, :system
-set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl foreman}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
 set :log_level, :warn
 
 # Rails
@@ -18,13 +18,22 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 
 namespace :app do
+  desc "Quiet sidekiq"
+  task :quiet do
+    on roles(:app) do |host|
+      within release_path do
+        execute :sudo, :systemctl, :kill, "--signal TSTP", "'app-worker@*.service'"
+      end
+    end
+  end
+
   desc "Start web server"
   task :start do
     on roles(:app) do |host|
       within release_path do
-        execute :sudo, :systemctl, :start, "app-web@5000.service"
-        execute :sudo, :systemctl, :start, "app-worker@5001.service"
-        execute :sudo, :systemctl, :start, "app-clock@5002.service"
+        execute :sudo, :systemctl, :start, "'app-web@*.service'"
+        execute :sudo, :systemctl, :start, "'app-worker@*.service'"
+        execute :sudo, :systemctl, :start, "'app-clock@*.service'"
       end
     end
   end
@@ -33,9 +42,9 @@ namespace :app do
   task :stop do
     on roles(:app) do |host|
       within release_path do
-        execute :sudo, :systemctl, :stop, "app-web@5000.service"
-        execute :sudo, :systemctl, :stop, "app-worker@5001.service"
-        execute :sudo, :systemctl, :stop, "app-clock@5002.service"
+        execute :sudo, :systemctl, :stop, "'app-web@*.service'"
+        execute :sudo, :systemctl, :stop, "'app-worker@*.service'"
+        execute :sudo, :systemctl, :stop, "'app-clock@*.service'"
       end
     end
   end
@@ -44,23 +53,35 @@ namespace :app do
   task :restart do
     on roles(:app) do |host|
       within release_path do
-        execute :sudo, :systemctl, :restart, "app-web@5000.service"
-        execute :sudo, :systemctl, :restart, "app-worker@5001.service"
-        execute :sudo, :systemctl, :restart, "app-clock@5002.service"
+        execute :sudo, :systemctl, :restart, "'app-web@*.service'"
+        execute :sudo, :systemctl, :restart, "'app-worker@*.service'"
+        execute :sudo, :systemctl, :restart, "'app-clock@*.service'"
       end
     end
   end
 
-  desc "Reload systemd"
-  task :systemd do
+  desc "Reload"
+  task :reload do
     on roles(:app) do
       within release_path do
-        execute :sudo, :foreman, :export, :systemd, "/etc/systemd/system", "--user app"
+        execute :sudo, :systemctl, :kill, "-s SIGUSR1", "--kill-who=main", "'app-web@*.service'"
+        execute :sudo, :systemctl, :restart, "'app-worker@*.service'"
+        execute :sudo, :systemctl, :restart, "'app-clock@*.service'"
+      end
+    end
+  end
+
+  desc "Export systemd"
+  task :export do
+    on roles(:app) do
+      within release_path do
+        execute :sudo, "/usr/local/rbenv/shims/foreman", :export, :systemd, "/etc/systemd/system", "--user app"
         execute :sudo, :systemctl, "daemon-reload"
       end
     end
   end
+
 end
 
-after 'deploy:published', 'app:systemd'
-after 'app:systemd', 'app:restart'
+before "deploy", "app:quiet"
+after "deploy:published", "app:reload"
