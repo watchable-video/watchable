28d5a8e23dbf3450c6a9541f20e6e1872c24b017
Ben Ubois <ben@benubois.com>
2018-03-18 13:25:21 -0700

    Template updates.

---
+++ app/assets/javascripts/tv/views/shared/search_results_view.js
@@ -0,0 +1,43 @@
+class SearchResultsView extends View {
+
+  constructor(query, title) {
+    super();
+    this.query = query
+    this.title = title || ""
+    this.markup = ""
+  }
+
+  template() {
+    return `
+    <grid id="${this._id()}">
+      ${this._innerHTML()}
+    </grid>`;
+  }
+
+  _id() {
+    return this.constructor.name;
+  }
+
+  _innerHTML() {
+    return `
+    <header>
+      <title><![CDATA[${this.title}]]></title>
+    </header>
+    <section>
+      ${this.markup}
+    </section>`;
+  }
+
+  update() {
+    const uri = url("search");
+    uri.addSearch({q: this.query});
+    request("GET", uri).then((response) => {
+      data.videos = JSON.parse(response).map(data => new Video(data));
+      this.markup = data.videos
+        .map((video, index) => new VideoLockupView(video, "SearchResult").template())
+        .join("");
+      super.update()
+    });
+  }
+
+}
