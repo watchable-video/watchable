e78679af2aaf8c6086c5102a2eaec804fb018912
Ben Ubois <ben@benubois.com>
2018-03-22 18:44:04 -0700

    Separate create from update so callbacks fire.

--- app/jobs/refresh_job.rb
+++ app/jobs/refresh_job.rb
@@ -3,16 +3,26 @@ class RefreshJob < ApplicationJob
 
   discard_on StandardError
 
+  attr_reader :account, :client
+
   def perform(account)
-    google = GoogleClient.new(account.cloudkit_id)
-    playlist_ids = google.subscribed_playlist_ids
-    playlist_ids.each do |playlist_id|
-      videos = google.channel_videos(playlist_id).map do |data|
-        Video.new_from_api(account, data)
-      end
-      Video.import videos, on_duplicate_key_update: {conflict_target: [:cloudkit_id, :youtube_id], columns: [:data]}
+    @account = account
+    @client = GoogleClient.new(account.cloudkit_id)
+
+    client.subscribed_playlist_ids.each do |playlist_id|
+      create_and_update_videos(playlist_id)
     end
     account.update(initial_sync_complete: true)
+  end
+
+  def create_and_update_videos(playlist_id)
+    videos = client.channel_videos(playlist_id).each_with_object([]) do |data, updates|
+      video = Video.new_from_api(account, data)
+      video.save!
+    rescue ActiveRecord::RecordNotUnique
+      updates.push video
+    end
+    Video.import videos, on_duplicate_key_update: {conflict_target: [:cloudkit_id, :youtube_id], columns: [:data]}
   end
 
 end
