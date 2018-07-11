cdb3c8bbdbf14f0be1b0e21e14e66ec396138675
Ben Ubois <ben@benubois.com>
2018-07-11 11:19:44 -0700

    Clean title/subtitle.

--- app/models/video.rb
+++ app/models/video.rb
@@ -57,7 +57,24 @@ class Video < ApplicationRecord
     end
   end
 
+  def title
+    original = title_parts.first || self.data.dig("snippet", "title")
+    original.sub("#{self.data.dig("snippet", "channel_title")}: ", "")
+  end
+
+  def subtitle
+    title_parts.drop(1) - [self.data.dig("snippet", "channel_title")]
+  end
+
   private
+
+    def title_parts
+      if title = self.data.dig("snippet", "title")
+        title.split(/\s[\|-—]\s/)
+      else
+        []
+      end
+    end
 
     def cache_key
       if persisted?
