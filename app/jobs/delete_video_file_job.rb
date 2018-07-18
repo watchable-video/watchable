9f0e1d3ff0e7056892565cbd4e92af49dc4bb742
Ben Ubois <ben@benubois.com>
2018-07-18 16:54:19 -0700

    Correct path.

--- app/jobs/delete_video_file_job.rb
+++ app/jobs/delete_video_file_job.rb
@@ -5,7 +5,8 @@ class DeleteVideoFileJob < ApplicationJob
 
   def perform
     Video.where(watched: true).find_each do |video|
-      path = Rails.root.join(*video.file_path)
+      path = Rails.root.join(*["public", *video.file_path])
+
       if video.file_path.present? && File.exist?(path)
         FileUtils.rm(path)
       end
--- app/jobs/process_video_job.rb
+++ app/jobs/process_video_job.rb
@@ -17,38 +17,38 @@ class ProcessVideoJob < ApplicationJob
 
   private
 
-  def filename
-    Digest::SHA1.hexdigest(video.youtube_id).ext("mp4")
-  end
-
-  def destination_path
-    ["public", "videos", filename]
-  end
-
-  def full_destination_path
-    Rails.root.join(*destination_path)
-  end
-
-  def download
-    filename = "#{SecureRandom.hex}.mp4"
-    path = File.join("/", "tmp", filename)
-    command = [
-      "youtube-dl",
-      "--quiet",
-      "--format",
-      "'bestvideo[height<=?1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'",
-      "--merge-output-format",
-      "mp4",
-      "--output",
-      "#{path}",
-      "#{video.youtube_id}",
-    ]
-
-    if system(command.join(" ")) && File.exist?(path)
-      path
-    else
-      nil
+    def filename
+      Digest::SHA1.hexdigest(video.youtube_id).ext("mp4")
     end
-  end
+
+    def destination_path
+      ["videos", filename]
+    end
+
+    def full_destination_path
+      Rails.root.join(*["public", *destination_path])
+    end
+
+    def download
+      filename = "#{SecureRandom.hex}.mp4"
+      path = File.join("/", "tmp", filename)
+      command = [
+        "youtube-dl",
+        "--quiet",
+        "--format",
+        "'bestvideo[height<=?1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio'",
+        "--merge-output-format",
+        "mp4",
+        "--output",
+        "#{path}",
+        "#{video.youtube_id}",
+      ]
+
+      if system(command.join(" ")) && File.exist?(path)
+        path
+      else
+        nil
+      end
+    end
 
 end
